e746d65557f36763807ee4fff539ac2ee0c3e4ca
Albert0espejo <albertowere12@outlook.com>
2018-06-02 20:04:41 +0200

    algoi

--- ChatUDP/src/es/uma/informatica/rsd/chat/impl/ComunicacionImpl.java
+++ ChatUDP/src/es/uma/informatica/rsd/chat/impl/ComunicacionImpl.java
@@ -47,13 +47,13 @@ public class ComunicacionImpl implements Comunicacion {
 				String[] sc = msj.split("!"); 
 				
 				if(sc[0].isEmpty()) {//unicast
-					if(sc[1]!=alias) {
+					if(!(sc[1].equals(alias))) {
 						System.out.println("No es mi mensaje");
 						controlador.mostrarMensaje(new InetSocketAddress(dp.getAddress(), dp.getPort()), sc[1], sc[2]);
 					}
 				}else {//multicast
 					InetAddress dirReal = InetAddress.getByName(sc[0]);
-					if(sc[1]!=alias) {
+					if(!(sc[1].equals(alias))) {
 						System.out.println("No es mi mensaje");
 						controlador.mostrarMensaje(new InetSocketAddress(dirReal, dp.getPort()), sc[1], sc[2]);
 					}
